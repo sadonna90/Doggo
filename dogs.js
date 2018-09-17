b39fcdcb97bc9f81d0eb27ec57a07ee737dc3bc9
Tony Hudgins <tonyhudgins@gmail.com>
2018-09-17 02:33:00 -0700

    Bug Fixes no new functionality

--- dogs.js
+++ dogs.js
@@ -18,16 +18,18 @@ function getDogNames(){
 var pics;
 var dogPics;
 
-fetch('https://api.thedogapi.com/v1/images/search?format=src&mime_types=image/gif')
-  .then(response => response.json())
-  .then(data => {
-    pics = data;
-    //dogPics = pic.map(pic => pic.name).join(",<br>");
-    getDogPics();
-});
+// fetch('https://api.thedogapi.com/v1/images/search?format=src&mime_types=image/gif')
+//   .then(response => response.blob())  //changed to appropriate data type for images
+//   .then(data => {
+//     pics = data;
+//     //dogPics = pic.map(pic => pic.name).join(",<br>");
+//     getDogPics();
+// });
 
 function getDogPics(){
-  document.getElementById("dogGif").innerHTML = dogPics;
+//  document.getElementById("dogGif").innerHTML = dogPics;
+  document.getElementById("dogGif").innerHTML = `<img src = "https://api.thedogapi.com/v1/images/search?format=src&mime_types=image/gif"\>`;
 }
 
-{"id":"rkiByec47","url":"https://cdn2.thedogapi.com/images/rkiByec47_640.jpg","breeds":[{"id":3,"name":"African Hunting Dog","life_span":"11 years","bred_for":"A wild pack animal","temperament":"Wild, Hardworking, Dutiful","weight":{"imperial":"44 - 66 lbs","metric":"20 - 30 kgs"},"height":{"imperial":"30 inches at the withers","metric":"76 cm at the withers"}}],"categories":[]}
+// added sampleJSON variable to fix error where Object was interpreted as a malformed function.
+var sampleJSON = {"id":"rkiByec47","url":"https://cdn2.thedogapi.com/images/rkiByec47_640.jpg","breeds":[{"id":3,"name":"African Hunting Dog","life_span":"11 years","bred_for":"A wild pack animal","temperament":"Wild, Hardworking, Dutiful","weight":{"imperial":"44 - 66 lbs","metric":"20 - 30 kgs"},"height":{"imperial":"30 inches at the withers","metric":"76 cm at the withers"}}],"categories":[]}
